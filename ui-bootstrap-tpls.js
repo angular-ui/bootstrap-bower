bdd1cbc50f5dd9c097f0b3e7806da4681c54202a
Umayr Shahid <umayrr@hotmail.co.uk>
2015-04-24 04:39:10 +0500

    Patch for accordion directive.
    Replaced simple `href` to `href="javascript:void(0)"` as it was messing up the current route.

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -3905,7 +3905,7 @@ angular.module("template/accordion/accordion-group.html", []).run(["$templateCac
     "<div class=\"panel panel-default\">\n" +
     "  <div class=\"panel-heading\">\n" +
     "    <h4 class=\"panel-title\">\n" +
-    "      <a href class=\"accordion-toggle\" ng-click=\"toggleOpen()\" accordion-transclude=\"heading\"><span ng-class=\"{'text-muted': isDisabled}\">{{heading}}</span></a>\n" +
+    "      <a href=\"javascript:void(0)\" class=\"accordion-toggle\" ng-click=\"toggleOpen()\" accordion-transclude=\"heading\"><span ng-class=\"{'text-muted': isDisabled}\">{{heading}}</span></a>\n" +
     "    </h4>\n" +
     "  </div>\n" +
     "  <div class=\"panel-collapse\" collapse=\"!isOpen\">\n" +
